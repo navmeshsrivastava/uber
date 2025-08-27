087a3792e311c57d4bf662dcbbf09a6ab070af03
Navmesh Srivastava <navmeshsrivastav815@gmail.com>
2025-08-27 11:47:49 +0530

    Login and Signup pages for Captain and User done

---
+++ frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const CaptainLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [captainData, setCaptainData] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setCaptainData({
+      email,
+      password,
+    });
+    console.log(captainData);
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="p-7 flex h-screen flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
+          alt=""
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">What's your email?</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            type="email"
+            placeholder="john@example.com"
+          />
+          <h3 className="text-lg font-medium mb-2">Enter password</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="Password"
+          />
+          <button className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2  w-full text-lg placeholder:text-base">
+            Login
+          </button>
+          <p className="text-center">
+            Join a fleet?{' '}
+            <Link to="/captain-signup" className="text-blue-600">
+              Register as a captain
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div>
+        <Link
+          to="/login"
+          className="bg-[#d5622d] flex items-center justify-center  text-white font-semibold mb-5 rounded px-4 py-2  w-full text-lg placeholder:text-base"
+        >
+          Sign in as User
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainLogin;
---
+++ frontend/src/pages/UserSignup.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const UserSignup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userData, setUserData] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setUserData({
+      fullname: {
+        firstname: firstName,
+        lastname: lastName,
+      },
+      email,
+      password,
+    });
+    setEmail('');
+    setPassword('');
+    setFirstName('');
+    setLastName('');
+  };
+
+  return (
+    <div className="p-7 flex h-screen flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
+          alt=""
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">What's your name?</h3>
+          <div className="flex gap-4 mb-6">
+            <input
+              className="bg-[#eeeeee] w-1/2 rounded px-4 py-2  text-lg placeholder:text-base"
+              required
+              type="text"
+              placeholder="First name"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+            <input
+              className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 text-lg placeholder:text-base"
+              required
+              type="text"
+              placeholder="Last name"
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
+          </div>
+          <h3 className="text-lg font-medium mb-2">What's your email?</h3>
+          <input
+            className="bg-[#eeeeee] mb-6 rounded px-4 py-2 w-full text-lg placeholder:text-base"
+            required
+            type="email"
+            placeholder="john@example.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <h3 className="text-lg font-medium mb-2">Enter password</h3>
+          <input
+            className="bg-[#eeeeee] mb-6 rounded px-4 py-2 w-full text-lg placeholder:text-base"
+            required
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2  w-full text-base placeholder:text-sm">
+            Login
+          </button>
+          <p className="text-center">
+            Already have an account?{' '}
+            <Link to="/login" className="text-blue-600">
+              Login here
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div>
+        <p className="text-[10px] leading-tight">
+          This site is protected by reCAPTCHA and the{' '}
+          <span className="underline">Google Privacy Policy</span> and{' '}
+          <span className="underline">Terms of Service</span> apply.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserSignup;
